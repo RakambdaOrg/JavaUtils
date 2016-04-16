cd142228eeba85f7ce5e6c1f8fcd43e86b0a9ed8
Thomas Couchoud <zerderr@gmail.com>
2016-04-16 10:47:15 +0200

    Add sanitizeFileName into FileUtils

--- src/main/java/fr/mrcraftcod/utils/FileUtils.java
+++ src/main/java/fr/mrcraftcod/utils/FileUtils.java
@@ -1,6 +1,7 @@
 package fr.mrcraftcod.utils;
 
 import java.io.File;
+import java.util.stream.Collectors;
 public class FileUtils
 {
 	public static File getAppDataFolder()
@@ -17,5 +18,10 @@ public class FileUtils
 	public static File getDesktopFolder()
 	{
 		return new File(System.getProperty("user.home"), "Desktop");
+	}
+
+	public static String sanitizeFileName(String name)
+	{
+		return name.chars().mapToObj(i -> (char) i).filter(c -> Character.isLetterOrDigit(c) || c == '-' || c == '_' || c == ' ').map(String::valueOf).collect(Collectors.joining());
 	}
 }

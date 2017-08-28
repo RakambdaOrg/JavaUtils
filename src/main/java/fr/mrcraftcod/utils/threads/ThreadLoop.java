9a28c586b71e354ce600b88cab8a340e57467848
Thomas Couchoud <zerderr@gmail.com>
2017-08-28 14:14:28 +0200

    Add onStart for ThreadLoop

--- src/main/java/fr/mrcraftcod/utils/threads/ThreadLoop.java
+++ src/main/java/fr/mrcraftcod/utils/threads/ThreadLoop.java
@@ -2,14 +2,16 @@ package fr.mrcraftcod.utils.threads;
 
 import fr.mrcraftcod.utils.Log;
 import javafx.beans.property.SimpleBooleanProperty;
+
 public abstract class ThreadLoop extends Thread
 {
 	private SimpleBooleanProperty running = new SimpleBooleanProperty(true);
 	private SimpleBooleanProperty pause = new SimpleBooleanProperty(false);
-
+	
 	@Override
 	public void run()
 	{
+		onStart();
 		while(!this.isInterrupted() && this.isRunning())
 			try
 			{
@@ -24,43 +26,49 @@ public abstract class ThreadLoop extends Thread
 				break;
 			}
 	}
-
+	
+	public void onStart()
+	{
+	}
+	
 	public boolean isRunning()
 	{
 		return this.runningProperty().get();
 	}
-
+	
 	public SimpleBooleanProperty runningProperty()
 	{
 		return this.running;
 	}
-
+	
 	public boolean isPaused()
 	{
 		return this.pauseProperty().get();
 	}
-
+	
 	public SimpleBooleanProperty pauseProperty()
 	{
 		return this.pause;
 	}
-
+	
 	public void close()
 	{
 		this.interrupt();
-		this.running .set(false);
+		this.running.set(false);
 		this.onClosed();
 	}
-
-	public void onClosed(){};
-
+	
+	public void onClosed(){}
+	
+	;
+	
 	public abstract void loop() throws Exception;
-
+	
 	public void pause()
 	{
 		this.pauseProperty().set(true);
 	}
-
+	
 	public void unpause()
 	{
 		this.pauseProperty().set(false);

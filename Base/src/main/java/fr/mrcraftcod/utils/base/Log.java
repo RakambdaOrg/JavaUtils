3c0d659ef7f4c7eabaa226c93778071ecbc7e403
Thomas Couchoud <zerderr@gmail.com>
2018-07-10 20:57:06 +0200

    _

--- Base/src/main/java/fr/mrcraftcod/utils/base/Log.java
+++ Base/src/main/java/fr/mrcraftcod/utils/base/Log.java
@@ -1,89 +1,98 @@
 package fr.mrcraftcod.utils.base;
 
 import java.util.ArrayList;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class Log
 {
-	private static Logger logger;
 	private static final ArrayList<LogListener> listeners = new ArrayList<>();
-
+	private static Logger logger;
+	
 	public interface LogListener
 	{
 		void onLogMessage(Level level, String message);
-
+		
 		void onLogMessage(Level level, String message, Throwable throwable);
 	}
-
+	
 	public static void addListener(LogListener listener)
 	{
 		listeners.add(listener);
 	}
-
-	public static Logger getLogger()
+	
+	public static void warning(String format, Object... args)
 	{
-		return logger != null ? logger : setAppName("MCCUtils");
+		warning(String.format(format, args));
 	}
-
-	public static Logger setAppName(String name)
-	{
-		logger = Logger.getLogger(name);
-		return logger;
-	}
-
+	
 	public static void warning(String s)
 	{
 		log(Level.WARNING, s);
 	}
-
-	public static void warning(String s, Throwable e)
-	{
-		log(Level.WARNING, s, e);
-	}
-
-	public static void warning(boolean log, String s, Throwable e)
-	{
-		if(log)
-			warning(s, e);
-	}
-
-	public static void info(String s)
-	{
-		log(Level.INFO, s);
-	}
-
-	public static void info(boolean log, String s)
-	{
-		if(log)
-			info(s);
-	}
-
-	public static void error(String s)
-	{
-		log(Level.SEVERE, s);
-	}
-
-	public static void error(String s, Throwable e)
-	{
-		log(Level.SEVERE, s ,e);
-	}
-
-	public static void error(boolean log, String s, Throwable e)
-	{
-		if(log)
-			error(s, e);
-	}
-
+	
 	public static void log(Level level, String s)
 	{
 		getLogger().log(level, s);
 		listeners.forEach(logListener -> logListener.onLogMessage(level, s));
 	}
-
+	
+	public static Logger getLogger()
+	{
+		return logger != null ? logger : setAppName("MCCUtils");
+	}
+	
+	public static Logger setAppName(String name)
+	{
+		logger = Logger.getLogger(name);
+		
+		ConsoleHandler ch = new ConsoleHandler();
+		ch.setLevel(Level.INFO);
+		ch.setFormatter(new LoggerFormatter());
+		logger.addHandler(ch);
+		
+		return logger;
+	}
+	
+	public static void warning(Throwable e, String format, Object... args)
+	{
+		warning(e, String.format(format, args));
+	}
+	
+	public static void warning(Throwable e, String s)
+	{
+		log(Level.WARNING, s, e);
+	}
+	
 	public static void log(Level level, String s, Throwable e)
 	{
 		getLogger().log(level, s, e);
 		listeners.forEach(logListener -> logListener.onLogMessage(level, s, e));
-
+	}
+	
+	public static void info(String format, Object... args)
+	{
+		info(String.format(format, args));
+	}
+	
+	public static void info(String s)
+	{
+		log(Level.INFO, s);
+	}
+	
+	public static void error(String s)
+	{
+		log(Level.SEVERE, s);
+	}
+	
+	public static void error(Throwable e, String format, Object... args)
+	{
+		error(e, String.format(format, args));
+	}
+	
+	public static void error(Throwable e, String s)
+	{
+		log(Level.SEVERE, s, e);
 	}
 }

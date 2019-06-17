1dd4a63cd4288c34120e9136d675f2c610a917ae
Thomas Couchoud <thomas.couchoud@gmail.com>
2019-06-17 11:58:01 +0200

    Attempt ci

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,5 +1,5 @@
 if [[ ${CI_COMMIT_MESSAGE} =~ ^\[maven-release-plugin\].+ ]]; then
   echo "Not deploying this commit"
 else
-  mvn ${MAVEN_CLI_OPTS} release:clean release:prepare release:perform release:clean
+  mvn ${MAVEN_CLI_OPTS} deploy
 fi;

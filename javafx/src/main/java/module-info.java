7ae4ba2f68ac3ec3c81a31edd84799b793940dfd
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-01-11 15:52:46 +0100

    Mark javafx dependencies as transitive & update dependencies

--- javafx/src/main/java/module-info.java
+++ javafx/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 open module fr.raksrinana.utils.javafx {
-	requires javafx.base;
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.swing;
+	requires transitive javafx.base;
+	requires transitive javafx.controls;
+	requires transitive javafx.graphics;
+	requires transitive javafx.swing;
 	requires fr.raksrinana.utils.base;
 	requires org.slf4j;
 	requires static lombok;

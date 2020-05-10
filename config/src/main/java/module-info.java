e584b85715f8a9661839c53952b45a0464dacabf
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-05-10 17:31:25 +0200

    Setup transitive dep

--- config/src/main/java/module-info.java
+++ config/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 open module fr.raksrinana.utils.config {
 	requires fr.raksrinana.utils.base;
 	requires java.sql;
-	requires sqlite.jdbc;
 	requires org.slf4j;
 	requires static lombok;
+	requires transitive sqlite.jdbc;
+	requires transitive com.zaxxer.hikari;
+	requires transitive com.h2database;
 	exports fr.raksrinana.utils.config;
-	requires com.zaxxer.hikari;
 }
